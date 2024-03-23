69d2dad1cd55b5f37cc70cdbb0802f68d0303ec2
itsahadul99 <itsahadul999@gmail.com>
2024-03-23 10:38:23 +0600

    added category job list data and show it on ui

--- src/components/JobCategoryList.jsx
+++ src/components/JobCategoryList.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 
 const JobCategoryList = () => {
+    const [joblist, setJobList] = useState([]);
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setJobList(data))
+    }, [])
+    console.log(joblist);
     return (
-        <div>
+        <div className="max-w-6xl mx-auto">
             <h1 className="text-5xl font-bold text-center">Job Category List</h1>
             <p className="text-sm font-semibold text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className="grid grid-cols-4 gap-5 my-10">
+            {
+                joblist.map(job => {
+                    return (
+                        <div key={job.id} className="card bg-base-100 shadow-sm bg-gradient-to-r from-[#7e8ffe10] to-[#9873ff10] border">
+                            <div className="card-body">
+                                <img width={50} src={job.logo} alt="" />
+                                <h2 className="card-title font-bold">{job.category_name}</h2>
+                                <p className="text-[#A3A3A3] font-medium">{job.availability}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            </div>
         </div>
     );
 };
